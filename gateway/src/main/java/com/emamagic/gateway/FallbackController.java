4aa3054458cbbb06afe27b13c4497bf7972df823
amirhossein <emam>
2025-03-04 14:02:44 +0330

    edit gateway

--- gateway/src/main/java/com/emamagic/gateway/FallbackController.java
+++ gateway/src/main/java/com/emamagic/gateway/FallbackController.java
@@ -2,6 +2,7 @@ package com.emamagic.gateway;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,8 @@ public class FallbackController {
     private final MessageSourceAccessor messageSourceAccessor;
 
     @GetMapping("/service-unavailable")
-    public BaseStatusResponse serviceUnavailable() {
-        System.out.println("Circuit Fallback Call........");
-        return new BaseStatusResponse(new BaseStatusResponse.Status(BaseStatusResponse.StatusCodes.ServiceUnavailable,
-                messageSourceAccessor.getMessage("CIRCUIT_FALLBACK_ERROR")));
+    public ResponseEntity<String> serviceUnavailable() {
+        return ResponseEntity.internalServerError().body(messageSourceAccessor.getMessage("CIRCUIT_FALLBACK_ERROR"));
     }
 
 }
